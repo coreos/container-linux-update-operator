3aa6258c8e8e2a2fabbfd873f3ac923c7f2bd38f
Euan Kemp <euan.kemp@coreos.com>
2017-03-16 15:00:43 -0700

    k8sutil: fix UpdateNodeRetry to handle conflicts
    Prior to this change it claimed to handle conflicts, but because it
    didn't 'Get' the new node, it would continue to try and submit the out
    of date update which caused a problem in the first place.
    
    This updates the logic, adds a test, and exports the function (since
    it's planned for broader use in an upcoming change).

---
+++ internal/k8sutil/metadata_test.go
@@ -0,0 +1,68 @@
+package k8sutil
+
+import (
+	"fmt"
+	"strconv"
+	"testing"
+
+	"github.com/golang/mock/gomock"
+
+	mock_v1 "github.com/coreos-inc/container-linux-update-operator/internal/k8sutil/mocks"
+	"k8s.io/client-go/pkg/api/errors"
+	"k8s.io/client-go/pkg/api/unversioned"
+	v1api "k8s.io/client-go/pkg/api/v1"
+)
+
+func atomicCounterIncrement(n *v1api.Node) {
+	counterAnno := "counter"
+	s := n.Annotations[counterAnno]
+	var i int
+	if s == "" {
+		i = 0
+	} else {
+		var err error
+		i, err = strconv.Atoi(s)
+		if err != nil {
+			panic(err)
+		}
+	}
+	n.Annotations[counterAnno] = strconv.Itoa(i + 1)
+}
+
+func TestUpdateNodeRetryHandlesConflict(t *testing.T) {
+	DefaultBackoff.Duration = 0
+	DefaultRetry.Duration = 0
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	mockNi := mock_v1.NewMockNodeInterface(ctrl)
+	mockNode := &v1api.Node{}
+	mockNode.SetName("mock_node")
+	mockNode.SetNamespace("default")
+	mockNode.SetAnnotations(map[string]string{"counter": "20"})
+	mockNode.SetResourceVersion("20")
+
+	mockNi.EXPECT().Get("mock_node").Return(mockNode, nil).AnyTimes()
+
+	// Conflict once; mock that a third party incremented the counter from '20'
+	// to '21' right after the node is returned
+	gomock.InOrder(
+		mockNi.EXPECT().Update(mockNode).Do(func(n *v1api.Node) {
+			// Fake conflict; the counter was incremented elsewhere; resourceVersion is now 21
+			mockNode.SetAnnotations(map[string]string{"counter": "21"})
+			mockNode.SetResourceVersion("21")
+		}).Return(mockNode, errors.NewConflict(unversioned.GroupResource{}, "mock_node", fmt.Errorf("err"))),
+
+		// And then the successful retry
+		mockNi.EXPECT().Update(mockNode).Return(mockNode, nil),
+	)
+
+	err := UpdateNodeRetry(mockNi, "mock_node", atomicCounterIncrement)
+
+	if err != nil {
+		t.Errorf("unexpected error: expected increment to succeed")
+	}
+	if mockNode.Annotations["counter"] != "22" {
+		t.Errorf("expected the counter to hit 22; was %v", mockNode.Annotations["counter"])
+	}
+}
